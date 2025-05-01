80b9fa21221fbddfc45952ee88f74a37a43229b3
Parag Bhosale <90536505+bhosaleparag@users.noreply.github.com>
2025-05-01 10:53:44 +0530

    [5] [Time Beats: 42.39%] [Memory Beats: 25.96%] - LeetPush

---
+++ Longest-Palindromic-Substring.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  if (s.length <= 1) return s;
+
+  let start = 0, end = 0;
+
+  const expandFromCenter = (left, right) => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return [left + 1, right - 1];
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let [left1, right1] = expandFromCenter(i, i);   
+    let [left2, right2] = expandFromCenter(i, i + 1); 
+
+    if (right1 - left1 > end - start) {
+      start = left1;
+      end = right1;
+    }
+
+    if (right2 - left2 > end - start) {
+      start = left2;
+      end = right2;
+    }
+  }
+
+  return s.slice(start, end + 1);
+};
