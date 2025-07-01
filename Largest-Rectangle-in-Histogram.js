3b7268e8724f9069af8c85c7ed7a991ceaf05fef
Parag Bhosale <90536505+bhosaleparag@users.noreply.github.com>
2025-07-01 14:16:25 +0530

    [84] [Time Beats: 76.02%] [Memory Beats: 45.83%] - LeetPush

--- Largest-Rectangle-in-Histogram.js
+++ Largest-Rectangle-in-Histogram.js
@@ -4,16 +4,16 @@
  */
 var largestRectangleArea = function(h) {
   let ans=0;
-  for(let p=0;p<h.length;p++){
-    if(h[p]===h[p-1]) continue
-    let lt=p, rt=p;
-    while(lt>0 && h[lt-1]>=h[p]){
-      lt--;
+  let st=[]
+  h.push(0)
+  for(let i=0;i<h.length;i++){
+    while(st.length && h[i] < h[st[st.length-1]]){
+      let pi = st.pop();
+      j = st.length===0 ? -1 : st[st.length-1]
+      width = i-j-1
+      ans = Math.max(ans, width*h[pi])
     }
-    while(rt<h.length && h[rt+1]>=h[p]){
-      rt++;
-    }
-    ans = Math.max(ans, (rt-lt+1)*h[p])
+    st.push(i)
   }
   return ans
 };
